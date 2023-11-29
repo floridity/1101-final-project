e943db0a8c4ecf029958e6fadc49c2ab5ec2b676
floridity <collinfernandez@gmail.com>
2023-11-29 16:31:50 -0600

    Added front-end code

--- app.py
+++ app.py
@@ -1,2 +1,11 @@
-# First Commit
-print("Hello World")
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+app.run(
+    debug=True
+)
